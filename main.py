b344a49eaa2a8a29e0f5cbd77f232cbcb4b95651
Varun Mutha <muthavarun07@gmail.com>
2026-02-03 16:19:07 +0530

    finish

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import datetime
-from fastapi import FastAPI # type: ignore
+from typing import Any
+from fastapi import FastAPI, HTTPException, Request, Response # type: ignore
 
 app = FastAPI(root_path="/api/v1")
 
 data : Any = [
     {
-     "campaign_id":"1",
+     "campaign_id":1,
      "name":"summer",
      "due_date" : datetime.datetime.now(),
      "created_at": datetime.datetime.now()
     },
     {
-     "campaign_id":"1",
-     "name":"summer",
-     "due_date" : datetime.datetime.now(),
-     "created_at": datetime.datetime.now()
-    },
-    {
-     "campaign_id":"2",
+     "campaign_id":2,
      "name":"monsoon",
      "due_date" : datetime.datetime.now(),
      "created_at": datetime.datetime.now()
     },
     {
-     "campaign_id":"3",
+     "campaign_id":3,
      "name":"winter",
      "due_date" : datetime.datetime.now(),
      "created_at": datetime.datetime.now()
@@ -37,3 +32,49 @@ async def read_root():
 @app.get("/campaigns")
 async def read_campaign():
     return {"campaigns": data}
+
+@app.get("/campaigns/{id}")
+async def read_campaign(id: int):
+    for campaign in data:
+        if campaign.get("campaign_id")  == id:
+            return {"campaign": campaign}
+    raise HTTPException(status_code=404)
+
+@app.post("/campaigns")
+def create_campaign(body: dict[str, Any]):
+    
+    new : Any = {
+        "campaign_id": 100,
+        "name":body.get("name"),
+        "created_at":datetime.datetime.now(),
+        "due_date": body.get("due_date")
+    }
+    
+    data.append(new)
+    return {"campaign": new}
+
+@app.put("/campaigns/{id}")
+async def update_campaigns(id:int ,body: dict[str,Any]):
+    
+    for index, campaign in enumerate(data):
+        if campaign.get("campaign_id") == id:
+            updated : Any = {
+                "camapaign_id": id,
+                "name": body.get("name"),
+                "due_date": body.get("due_date"),
+                "created_at": campaign.get("created_at")
+            }
+            data[index] = updated
+            return {"campaign": updated}
+    raise HTTPException(status_code=404)
+
+
+@app.delete("/campaigns/{id}")
+async def update_campaigns(id:int):
+    
+    for index, campaign in enumerate(data):
+        if campaign.get("campaign_id") == id:
+            data.pop(index)
+            return Response(status_code=204)
+    raise HTTPException(status_code=404)
+    
